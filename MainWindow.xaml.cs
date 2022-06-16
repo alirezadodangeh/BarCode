c6d445222a0dc8a3e91f1915a991faf4668c6bd0
alireza dodangeh <ardsoft_2010@yahoo.com>
2022-06-16 19:16:12 +0430

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BarCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+            b.IncludeLabel = true;  
+            var img = b.Encode(BarcodeLib.TYPE.CODE128, txt.Text, System.Drawing.Color.Black, System.Drawing.Color.White, 400, 70);
+            qrImage.Source = ConvertBitmap(img);
+        }
+        public BitmapImage ConvertBitmap(System.Drawing.Image Image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                Image.Save(ms, ImageFormat.Bmp);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = ms;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
